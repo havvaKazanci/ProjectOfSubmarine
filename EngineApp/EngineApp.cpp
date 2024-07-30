8e305d15dd8c1f8ee3857032dbb24b2e33bb11b4
havvaKazanci <havvacagla.kazanci20@ogr.atauni.edu.tr>
2024-07-30 14:40:01 +0300

    test

---
+++ EngineApp/EngineApp.cpp
@@ -0,0 +1,225 @@
+﻿#include <QApplication>
+#include <QWidget>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QTextEdit>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QFormLayout>
+#include <QPainter>
+#include <QPointF>
+#include <QTimer>
+#include <QDateTime>
+#include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsLineItem>
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsLineItem>
+#include <QGraphicsEllipseItem>
+#include <QPen>
+#include <QPointF>
+#include <QWidget>
+
+
+
+using namespace Qt;
+
+QList<float> AxisNumbersFromMessage(QString); //CREATES LIST XaxisNum , YaxisNum 
+void addNumberLabels(QGraphicsScene*, bool, qreal, qreal, qreal); //ADDS NUMBERS IN X AND Y AXIS
+QPointF SetQPointF(float, float); 
+
+
+int main(int argc, char* argv[])
+{
+   
+    QApplication app(argc, argv);
+
+    QTcpServer server;
+    server.listen(QHostAddress("10.10.14.132"), 1254);
+
+    QWidget destWindow;
+    destWindow.setWindowTitle("Engine Application");
+
+    QLabel* explanation = new QLabel("Welcome to the SubMerge", &destWindow);
+    QLabel* statusWri = new QLabel("Status of Engine: ", &destWindow);
+    QLabel* status = new QLabel("Engines are not running. System login is successful.", &destWindow);
+    QLabel* currentDest = new QLabel("Current Destination :", &destWindow);
+    QLabel* currentDestWri = new QLabel("(0,0)", &destWindow);
+    QLabel* writeLabel = new QLabel("", &destWindow);
+    QLabel* timeLabel = new QLabel("Time: ", &destWindow);
+
+    QTimer* timer = new QTimer(&destWindow);
+
+    QObject::connect(timer, &QTimer::timeout, [&]() {
+
+        QString currentTime = QDateTime::currentDateTime().toString("hh:mm:ss");
+        writeLabel->setText(currentTime);
+
+        });
+
+
+    timer->start(1000);
+
+    QString initialTime = QDateTime::currentDateTime().toString("hh:mm:ss");
+    writeLabel->setText(initialTime);
+
+    QTextEdit* directions = new QTextEdit();
+
+    QVBoxLayout* graphic = new QVBoxLayout();
+
+    QFormLayout* subLayoutDestWindow = new QFormLayout();
+
+    subLayoutDestWindow->addRow(explanation);
+    subLayoutDestWindow->addRow(timeLabel, writeLabel);
+    subLayoutDestWindow->addRow(statusWri, status);
+    subLayoutDestWindow->addRow(currentDest, currentDestWri);
+    subLayoutDestWindow->addRow(directions, graphic);
+
+
+    QVBoxLayout* destWindowLayout = new QVBoxLayout(&destWindow);
+    destWindowLayout->addLayout(subLayoutDestWindow);
+
+    QString comingData;
+
+    QGraphicsScene graphScene;
+
+    addNumberLabels(&graphScene, true, -850, 850, 50);
+    addNumberLabels(&graphScene, false, -850, 850, 50);
+
+
+    QGraphicsLineItem* xAxis = new QGraphicsLineItem(-850, 0, 850, 0);
+    xAxis->setPen(QPen(Qt::blue, 2));
+    graphScene.addItem(xAxis);
+
+    QGraphicsLineItem* yAxis = new QGraphicsLineItem(0, -850, 0, 850);
+    yAxis->setPen(QPen(Qt::blue, 2));
+    graphScene.addItem(yAxis);
+
+    
+    QGraphicsView* viewGraph = new QGraphicsView(&graphScene);
+
+    QList<float> axisNumbers;
+
+    QWidget* graphisc = new QWidget();
+
+    QList<QGraphicsEllipseItem*> pointItems;
+
+
+    QObject::connect(&server, &QTcpServer::newConnection, [&server, &status, &currentDestWri, &directions,&comingData, &destWindow,&axisNumbers,&graphScene,&pointItems]() {
+        QTcpSocket* socket = server.nextPendingConnection();
+        QObject::connect(socket, &QTcpSocket::readyRead, [socket, &status, &currentDestWri, &directions,&comingData, &destWindow,&axisNumbers,&graphScene,&pointItems]() {
+            QByteArray data = socket->readAll();
+            QString message = QString(data).trimmed();
+            comingData = message;
+            if (message == "START ENGINE") {
+                qDebug() << "ENGINE STARTED";
+                status->setText("ENGINE STARTED");
+
+            }
+            else if (message == "STOP ENGINE")
+            {
+                qDebug() << "ENGINE STOPPED";
+                status->setText("ENGINE STOPPED");
+
+            }
+            else
+            {
+                QString initialTime = QDateTime::currentDateTime().toString("hh:mm:ss");
+                qDebug() << message;
+                currentDestWri->setText(message);
+                directions->append(message + "    Instructions were given at time: " + initialTime);
+                axisNumbers= AxisNumbersFromMessage(comingData);
+                QPointF point(axisNumbers[0], axisNumbers[1]);
+
+                for (auto item : pointItems) {
+                    item->setBrush(Qt::blue); 
+                }
+
+                QGraphicsEllipseItem* pointItem = new QGraphicsEllipseItem(point.x() - 2, point.y() - 2, 4, 4);
+                pointItem->setBrush(Qt::red);
+                graphScene.addItem(pointItem);
+
+                QGraphicsTextItem* textItem = new QGraphicsTextItem(QString("(%1, %2)").arg(point.x()).arg(point.y()));
+                textItem->setPos(point.x() + 10, point.y() + 10); 
+                textItem->setDefaultTextColor(Qt::black); 
+                graphScene.addItem(textItem);
+
+                pointItems.append(pointItem);
+            }
+            });
+        });
+
+    graphic->addWidget(viewGraph);
+    viewGraph->setSceneRect(-850, -850, 1700, 1700);
+
+    destWindow.show();
+
+    return app.exec();
+
+}
+
+
+
+
+QPointF SetQPointF(float numX, float numY)
+{
+    QPointF point(0, 0);
+    point.setX(numX);
+    point.setY(numY);
+
+    return point;
+}
+
+void addNumberLabels(QGraphicsScene* scene, bool isXAxis, qreal start, qreal end, qreal step) {
+
+    for (qreal i = start; i <= end; i += step) {
+        QString label = QString::number(i, 'f', 1); 
+        QGraphicsTextItem* textItem = new QGraphicsTextItem(label);
+
+        if (isXAxis) {
+            textItem->setPos(i, 10); 
+            textItem->setTextWidth(80); 
+
+        }
+        else {
+            textItem->setPos(10, i); 
+            textItem->setTextWidth(80); 
+        }
+
+        textItem->setDefaultTextColor(Qt::black);
+        scene->addItem(textItem);
+    }
+}
+
+
+QList<float> AxisNumbersFromMessage(QString message)
+{
+
+    QString numberMessage = message;
+    numberMessage.remove('(').remove(')').trimmed();
+    QStringList numbers = numberMessage.split(',');
+
+    QList<float> result;
+
+    if (numbers.size() == 2) {
+        bool ok1, ok2;
+        float xAxisNum = numbers[0].toFloat(&ok1);
+        float yAxisNum = numbers[1].toFloat(&ok2);
+
+        if (ok1 && ok2) {
+            result.append(xAxisNum);
+            result.append(yAxisNum);
+        }
+    }
+    else
+    {
+        float xAxisNum = 0.0;
+        float yAxisNum = 0.0;
+    }
+
+    return result;
+   
+}
+
---
+++ EngineStartApp/EngineStartApp.cpp
@@ -0,0 +1,283 @@
+﻿
+#include <QApplication>
+#include <QWidget>
+#include <QFormLayout>
+#include <QHBoxLayout>
+#include <QString>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QRadioButton>
+#include <QGridLayout>
+#include <QObject>
+#include <QTcpSocket>
+#include <QTcpServer>
+#include <QMessageBox>
+#include <QPainter>
+#include <QPaintEvent>
+#include <QTextEdit>
+#include <QCloseEvent>
+#include <QEvent>
+#include <QDateTime>
+#include <QTimer>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <Qt>
+#include <QRegularExpression>
+#include <QCoreApplication>
+#include <QDebug>
+
+
+
+
+using namespace std; 
+
+
+bool IsValidFormat(const QString&);
+
+
+int main(int argc, char* argv[])
+{
+    QApplication app(argc, argv);  //INITILAZING A Q APPLICATION FIRST
+
+    QTcpSocket socket;
+    socket.connectToHost("10.10.14.132", 1254);     //CONNECTION PART
+
+    //INITILAZING LOGIN PAGE (FIRST PAGE)
+
+    QWidget loginWindow;
+    loginWindow.setWindowTitle("Welcome to SubMerge Navigator System");
+
+    QLabel* marketName = new QLabel("SubMerge Navigator Login System", &loginWindow);
+    QLabel* userId = new QLabel("User ID: ", &loginWindow);
+    QLabel* password = new QLabel("Password: ", &loginWindow);
+    QLabel* explanation = new QLabel("Login credentials are essential to initiate and maintain operational integrity within the submarine control system.", &loginWindow);
+
+    QLineEdit* userIdWri = new QLineEdit();
+    userIdWri->setPlaceholderText("example@sub.tr");
+    QLineEdit* passwordWri = new QLineEdit();
+    passwordWri->setPlaceholderText("Please login your password.");
+    passwordWri->setEchoMode(QLineEdit::Password);
+
+    QPushButton* loginButton = new QPushButton("LOG IN");
+
+    QFormLayout* formLayoutLogWindow = new QFormLayout(&loginWindow);
+    formLayoutLogWindow->addRow(marketName);
+    formLayoutLogWindow->addRow(userId, userIdWri);
+    formLayoutLogWindow->addRow(password, passwordWri);
+    formLayoutLogWindow->addRow(loginButton);
+    formLayoutLogWindow->addRow(explanation);
+
+
+    //END OF THE FIRST (LOGIN) PAGE INITILAZING
+
+    //SYSTEM WINDOW INITILAZING
+
+    QWidget systemWindow;
+    systemWindow.setWindowTitle("SubMerge Navigator System Properties");
+
+    QLabel* statusOfSubmarLabel = new QLabel("Status of Submarine: ", &systemWindow);
+    QLabel* currLocaOfSubmarLabel = new QLabel("Current Location of Submarine: ", &systemWindow);
+    QLabel* currLocaOfSubmarWri = new QLabel("(0,0)");
+    QLabel* engineMessage = new QLabel("INITIAL STATE ENGINE NOT WORKING!", &systemWindow);
+    QLabel* writeLabel = new QLabel("", &systemWindow);
+    QLabel* timeLabel = new QLabel("Time: ", &systemWindow);
+    //SYSTEM TIME PROCESSES
+
+    QTimer* timer = new QTimer(&systemWindow);
+
+    
+
+    QObject::connect(timer, &QTimer::timeout, [&]() {
+
+        QString currentTime = QDateTime::currentDateTime().toString("hh:mm:ss");
+        writeLabel->setText(currentTime);
+
+        });
+
+
+    timer->start(1000);
+
+    QString initialTime = QDateTime::currentDateTime().toString("hh:mm:ss");
+    writeLabel->setText(initialTime);
+
+    // SYSTEM TIME PROCESSES ENDING 
+
+    QPushButton* startButton = new QPushButton("START ENGINE");
+    QPushButton* stopButton = new QPushButton("STOP ENGINE");
+    QPushButton* changLocaButton = new QPushButton("CHANGE THE LOCATION OF SUBMARINE");
+    changLocaButton->setEnabled(false);
+    stopButton->setEnabled(false);
+
+    QFormLayout* layoutSystemWindow = new QFormLayout();
+    layoutSystemWindow->addRow(timeLabel, writeLabel);
+    layoutSystemWindow->addRow(statusOfSubmarLabel, engineMessage);
+    layoutSystemWindow->addRow(currLocaOfSubmarLabel, currLocaOfSubmarWri);
+    layoutSystemWindow->addRow(startButton);
+    layoutSystemWindow->addRow(changLocaButton);
+    layoutSystemWindow->addRow(stopButton);
+
+
+    QVBoxLayout* systemLayout = new QVBoxLayout(&systemWindow);
+    systemLayout->addLayout(layoutSystemWindow);
+
+    //SYSTEM WINDOW INITILAZING END
+
+    //DEST WINDOW (THIRD PAGE)
+
+    QWidget destWindow;
+    destWindow.setWindowTitle("Process Page Of Submarine");
+
+    QLabel* currentLoca = new QLabel("Current Location Of Submarine: ");
+    QLabel* currentLocaWri = new QLabel("(0,0)");
+    QLabel* targetDest = new QLabel("Target Destination Of Submarine: ");
+
+
+    QLineEdit* targetDestWri = new QLineEdit();
+    targetDestWri->setPlaceholderText("Please write the Target Location!");
+
+    QPushButton* goToDest = new QPushButton("Complete the Process!");
+    QPushButton* backButton = new QPushButton("Back");
+
+    QFormLayout* subLayoutOfDest = new QFormLayout();              
+    subLayoutOfDest->addRow(currentLoca, currentLocaWri);
+    subLayoutOfDest->addRow(targetDest, targetDestWri);
+    subLayoutOfDest->addRow(backButton, goToDest);
+
+    QVBoxLayout* processLayout = new QVBoxLayout(&destWindow);     
+    processLayout->addLayout(subLayoutOfDest);
+
+
+    //DEST WINDOW INITILAZING END
+
+
+
+    QObject::connect(loginButton, &QPushButton::clicked, [&]() {
+        QString userId = userIdWri->text();
+        QString password = passwordWri->text();
+
+
+        if (userId == "admin@sub.tr" && password == "password") {
+            loginWindow.hide();
+            systemWindow.show();
+        }
+        else {
+            QMessageBox::warning(&loginWindow, "Login Failed", "Invalid username or password");
+        }
+        });
+
+
+
+
+    QObject::connect(startButton, &QPushButton::clicked, [&socket, &engineMessage, startButton, &stopButton, &changLocaButton]() {
+
+        if (socket.state() == QAbstractSocket::ConnectedState) {
+            QString openStatu = "ENGINES ARE RUNNING!";
+            engineMessage->setText(openStatu);
+            QString startMessage = "START ENGINE";
+            socket.write(startMessage.toUtf8());
+            startButton->setEnabled(false);
+            stopButton->setEnabled(true);
+            changLocaButton->setEnabled(true);
+  
+
+        }
+        else {
+            QMessageBox::warning(nullptr, "Error!", "Not connected to server.");
+        }
+        });
+
+
+
+
+
+    QObject::connect(stopButton, &QPushButton::clicked, [&socket, &engineMessage, stopButton,&startButton, &changLocaButton]() {
+
+        if (socket.state() == QAbstractSocket::ConnectedState) {
+            QString closeStatu = "ENGINES ARE OFF!";
+            engineMessage->setText(closeStatu);
+            QString stopMessage = "STOP ENGINE";
+            socket.write(stopMessage.toUtf8());
+            stopButton->setEnabled(false);
+            startButton->setEnabled(true);
+            changLocaButton->setEnabled(false);
+            
+        }
+        else {
+            QMessageBox::warning(nullptr, "Error!", "Not connected to server.");
+        }
+        });
+
+
+
+
+    QObject::connect(changLocaButton, &QPushButton::clicked, [&]() {
+
+        systemWindow.hide();
+        destWindow.show();
+        });
+
+
+
+
+    QObject::connect(goToDest, &QPushButton::clicked, [&socket, &targetDestWri, &currentLocaWri, &currLocaOfSubmarWri]() {
+        QString currentDestination = currentLocaWri->text();
+        QString destination = targetDestWri->text();
+        if (!destination.isEmpty() && socket.state() == QAbstractSocket::ConnectedState ) {
+                
+                if (IsValidFormat(destination))
+                {
+                    if (destination == currentDestination)
+                    {
+                        QMessageBox::warning(nullptr, "Error!", "You are already at the destination you want to go to");
+                    }
+                    else
+                    {
+                        currLocaOfSubmarWri->setText(destination);
+                        currentLocaWri->setText(destination);
+                        QString message = destination;
+                        socket.write(message.toUtf8());
+                    }
+                }
+                else
+                {
+                    QMessageBox::warning(nullptr, "Error!", "You have written an wrong format destination!");
+                }
+           
+        }
+        else {
+            QMessageBox::warning(nullptr, "Error!", "The destination is not specified or is not connected to the server.");
+        }
+    });
+
+
+
+
+
+    QObject::connect(backButton, &QPushButton::clicked, [&]() {
+        systemWindow.show();
+        destWindow.hide();
+    });
+
+
+
+
+    loginWindow.show();
+
+    return app.exec();
+}
+
+
+bool IsValidFormat(const QString& input)    //FUNC FOR VALID INPUT CHECK IS LIKE ?(NUM,NUM)?
+{
+    QRegularExpression regex("^\\(\\d+,\\d+\\)$");
+
+    
+    QRegularExpressionMatch match = regex.match(input);
+
+    return match.hasMatch();
+}
+
